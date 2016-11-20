383ce854af5cb5d3417bee342f7e96bee33bab4a
byyMac <471073043@qq.com>
2016-11-20 19:27:06 +0800

    input float

--- dice.ts
+++ dice.ts
@@ -64,13 +64,13 @@ function setRandom(newRandom: () => number) {
 
 function parse (input: string) {
     let result = { times: 1, face: 6, modifier: 0 }
-    let match = input.match(/^\s*(\d+)?\s*[dD]\s*(\d+)\s*(.*?)\s*$/)
+    let match = input.match(/^\s*([\d\.]+)?\s*[dD]\s*([\d\.]+)\s*(.*?)\s*$/)
     if (match) {
         if (match[1]) {
-            result.times = parseInt(match[1])
+            result.times = parseFloat(match[1])
         }
         if (match[2]) {
-            result.face = parseInt(match[2])
+            result.face = parseFloat(match[2])
         }
         if (match[3]) {
             let modifiers = match[3].match(/([+-]\s*\d+)/g)
--- dice_test.js
+++ dice_test.js
@@ -1,3 +1,11 @@
+let _ = require('lodash')
+let assert = require('assert')
 let dice = require('./dice');
 
-console.log(dice('d30')())
+let d = dice('2d6.5')
+
+let result = _.times(100, function() {
+    return d()
+})
+console.log(result)
+assert(_.some(result, (v) => v === 13))
