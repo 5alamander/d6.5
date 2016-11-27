98244ec3598732805a8ba61133c9cd86b6ec9877
byyMac <471073043@qq.com>
2016-11-27 19:00:21 +0800

    add some test

--- dice_test.js
+++ dice_test.js
@@ -4,8 +4,11 @@ let dice = require('./dice');
 
 let d = dice('2d6.5')
 
-let result = _.times(100, function() {
-    return d()
+let result = _.times(10, function() {
+    return d(() => 0.99)
 })
-console.log(result)
-assert(_.some(result, (v) => v === 13))
+// console.log(result)
+assert(_.every(result, (v) => v === 13))
+
+console.log(dice('2d7 + 1')())
+console.log(dice('2d7')())
